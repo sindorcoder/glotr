a6636c81d7a44db597b7c30f01a7c6258fa51291
Sindor Olmasov <sindordev77@gmail.com>
2025-03-13 15:22:40 +0500

    shortcomings have been corrected, errors have been eliminated

--- app.js
+++ app.js
@@ -6,32 +6,41 @@ const closeCode = document.querySelector("#code_close");
 const btnSuccess = document.querySelector("#btn_success");
 const orderBack = document.querySelector("#order-back");
 const codeBack = document.querySelector("#code-back");
-const allBtn = document.querySelector("#allBtn")
+const allBtn = document.querySelector("#allBtn");
 btn.addEventListener("click", () => {
   codeChecker.style.transform = "translateX(0)";
+  codeChecker.style.display = "block";
 });
 
 closeCode.addEventListener("click", () => {
   codeChecker.style.transform = "translateX(100%)";
+  codeChecker.style.display = "block";
 });
 
 btnSuccess.addEventListener("click", () => {
   orderSuccess.style.transform = "translateX(0)";
   codeChecker.style.transform = "translateX(100%)";
+  codeChecker.style.display = "none";
+  orderSuccess.style.display = "block";
 });
 
 closeSuccess.addEventListener("click", () => {
   orderSuccess.style.transform = "translateX(100%)";
+  orderSuccess.style.display = "none";
 });
 orderBack.addEventListener("click", () => {
   orderSuccess.style.transform = "translateX(100%)";
+  orderSuccess.style.display = "none";
   codeChecker.style.transform = "translateX(0)";
+  codeChecker.style.display = "block";
 });
 codeBack.addEventListener("click", () => {
   codeChecker.style.transform = "translateX(100%)";
+  codeChecker.style.display = "none";
 });
 allBtn.addEventListener("click", () => {
   orderSuccess.style.transform = "translateX(100%)";
+  orderSuccess.style.display = "none";
 });
 
 document.addEventListener("DOMContentLoaded", function () {
